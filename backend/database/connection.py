f8ad584780687c3eef0b199fc8eef266aa28ca0d
PIYUSH JAIN <piyushjain250305@gmail.com>
2026-09-04 23:41:13 +0530

    Update connection.py

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise Runtime error("DATABASE_URL environment variable is not set")
+    raise RuntimeError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(
     DATABASE_URL,
